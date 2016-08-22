9f52c2bd967196280bf7121ad07a3e54ad87981e
Richard Bates <richo225@gmail.com>
2016-08-22 23:57:22 +0100

    Create restaurant model and db migration

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,10 +1,25 @@
 require 'rails_helper'
 
 feature "restaurants" do
-  scenario "prompts to add a restaurant" do
-    visit "/restaurants"
-    expect(page).to have_content "No restaurants yet"
-    expect(page).to have_link "Add a restaurant"
+
+  context "no restaurants added" do
+    scenario "prompts to add a restaurant" do
+      visit "/restaurants"
+      expect(page).to have_content ("No restaurants yet")
+      expect(page).to have_link ("Add a restaurant")
+    end
+  end
+
+  context "restaurants already added" do
+    before do
+      Restaurant.create(name: "KFC")
+    end
+
+    scenario "restaurants displayed" do
+      visit "/restaurants"
+      expect(page).to have_content "KFC"
+      expect(page).not_to have content "Add a restaurant"
+    end
   end
 
 end
