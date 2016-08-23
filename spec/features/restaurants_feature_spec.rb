2842da0a674967524a1e4b5c3c8bc3eef4deb893
Richard Bates <richo225@gmail.com>
2016-08-23 11:07:33 +0100

    Step 8 Creating a restaurant from form

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -14,11 +14,21 @@ feature "restaurants" do
     before do
       Restaurant.create(name: "KFC")
     end
-
     scenario "restaurants displayed" do
       visit "/restaurants"
       expect(page).to have_content "KFC"
       expect(page).not_to have_content "No restaurants yet"
+    end
+  end
+
+  context "creating restaurants" do
+    scenario "prompts user to fill in a form and displays the new restaurant" do
+      visit "/restaurants"
+      click_link "Add a restaurant"
+      fill_in "Name", with: "KFC"
+      click_button "Create Restaurant"
+      expect(page).to have_content "KFC"
+      expect(current_path).to eq "/restaurants"
     end
   end
 
