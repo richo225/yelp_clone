c172fff679aad57e144a2e5622b3a035f3a67ce2
BartJudge <BartJudge@gmail.com>
2016-08-23 19:02:07 +0100

    associations with reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160823144613) do
-
+ActiveRecord::Schema.define(version: 20160823172427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,4 +24,15 @@ ActiveRecord::Schema.define(version: 20160823144613) do
     t.text     "description"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text     "thoughts"
+    t.integer  "rating"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
+  end
+
+  add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
+
+  add_foreign_key "reviews", "restaurants"
 end
