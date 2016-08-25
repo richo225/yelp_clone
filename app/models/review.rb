469db5c1bcf8a26fd465265a3a35a54def75f495
Richard Bates <richo225@gmail.com>
2016-08-25 13:02:17 +0100

    Add build_review method to implement associations

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,12 @@ class Review < ApplicationRecord
 
   validates :rating, inclusion: (1..5)
   belongs_to :restaurant
-  
+  validates :user, uniqueness: { scope: :restaurant, message: "has reviewed this restaurant already" }
+
+  def build_review(attributes = {}, user)
+    review = reviews.build(attributes)
+    review.user = user
+    review
+  end
+
 end
