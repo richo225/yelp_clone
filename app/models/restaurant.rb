f405f8fa9c62baae4800f8303afc8a2c9093ba82
Richard Bates <richo225@gmail.com>
2016-08-25 15:40:26 +0100

    Add user/restaurant migration. User can't review a restaurant more than once

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,10 @@
 class Restaurant < ActiveRecord::Base
 
-  validates :name, length: { minimum: 3 }, uniqueness: true
-
+  belongs_to :user
   has_many :reviews,
         -> { extending WithUserAssociationExtension },
         dependent: :destroy
+
+  validates :name, length: { minimum: 3 }, uniqueness: true
 
 end
